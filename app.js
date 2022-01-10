54378f9ec1103e7d4effd07d5a808a3738a0c386
nguyenson-255 <nguyenson-255@github.com>
2022-01-10 20:21:54 +0700

    alo

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(function (req,res,next){
 
 })
 
-app.use('/', indexRouter);
+app.use('/home', indexRouter);
 app.use('/', loginRouter);
 app.use('/users', usersRouter);
 app.use('/checkout', loggedInUserGuard ,checkoutRouter);
